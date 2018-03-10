26bcba1d64228a9f292ce7dd5bdc61d287044d53
Madhav Datt <madhav.datt@hotmail.com>
2018-03-10 03:09:11 +0100

    Update aiuto.py

--- aiuto.py
+++ aiuto.py
@@ -106,4 +106,132 @@ def tweet_to_supporters(lang, lat, long, ref_id):
     pp.pprint(handles)
 
 
-   
+while True:
+    ref_id += 1
+    time.sleep(1)
+    messages = client.messages.list()
+    if messages[0].direction == 'inbound':
+        text_to = messages[0].from_
+        hi_text = messages[0].body.strip().split()[0]
+        ref_lang = translator.detect(hi_text).lang
+
+        translated_hi_msg = translator.translate(hi_msg, dest=ref_lang)
+        hi_msg_command = twilio_command.format(
+            account_sid=account_sid,
+            auth_token=auth_token,
+            text_to=text_to,
+            content=translated_hi_msg.text)
+        os.system(hi_msg_command)
+
+        while True:
+            time.sleep(5)
+            new_msg = client.messages.list()[0]
+            prev_msg = client.messages.list()[1]
+            if new_msg.direction == 'inbound' \
+                    and prev_msg.direction == 'outbound-api':
+                break
+
+        first_name = new_msg.body.strip().split()[0]
+        translated_help_msg = translator.translate(
+            help_msg.format(first_name=first_name), dest=ref_lang)
+        help_msg_command = twilio_command.format(
+            account_sid=account_sid,
+            auth_token=auth_token,
+            text_to=text_to,
+            content=translated_help_msg.text)
+        os.system(help_msg_command)
+
+        while True:
+            time.sleep(5)
+            new_msg = client.messages.list()[0]
+            prev_msg = client.messages.list()[1]
+            if new_msg.direction == 'inbound' \
+                    and prev_msg.direction == 'outbound-api':
+                break
+        request = translator.translate(new_msg.body, dest=ENGLISH)
+
+        translated_location_msg = translator.translate(
+            location_msg, dest=ref_lang)
+        location_msg_command = twilio_command.format(
+            account_sid=account_sid,
+            auth_token=auth_token,
+            text_to=text_to,
+            content=translated_location_msg.text)
+        os.system(location_msg_command)
+
+        while True:
+            time.sleep(5)
+            new_msg = client.messages.list()[0]
+            prev_msg = client.messages.list()[1]
+            if new_msg.direction == 'inbound' \
+                    and prev_msg.direction == 'outbound-api':
+                num_msgs = len(client.messages.list())
+                break
+
+        geolocator = Nominatim()
+        location = geolocator.geocode(new_msg.body, timeout=1000000)
+        lat, long = location.latitude, location.longitude
+
+        target_lang = 'en'
+        meet_keywords = REQUEST_KEYWORDS_LANGUAGE.intersection(
+            request.text.split())
+        if len(meet_keywords) != 0:
+            target_lang = ref_lang
+        tweet_to_supporters(target_lang, lat, long, ref_id)
+
+        translated_confirm_msg = translator.translate(
+            confirm_msg, dest=ref_lang)
+        confirm_msg_command = twilio_command.format(
+            account_sid=account_sid,
+            auth_token=auth_token,
+            text_to=text_to,
+            content=translated_confirm_msg.text)
+        os.system(confirm_msg_command)
+
+        while True:
+
+            while True:
+                time.sleep(5)
+                msg_count = len(client.messages.list())
+                sup_msg = client.messages.list()[0]
+                prev_msg = client.messages.list()[1]
+
+                if sup_msg.direction == 'inbound' \
+                        and prev_msg.direction == 'outbound-api' \
+                        and msg_count > num_msgs + 1:
+                    break
+
+            sup_number = sup_msg.from_
+            ref_number = text_to
+
+            translated_sup_msg = translator.translate(
+                sup_msg.body, dest=ref_lang)
+            sup_to_ref_msg_command = twilio_command.format(
+                account_sid=account_sid,
+                auth_token=auth_token,
+                text_to=ref_number,
+                content=translated_sup_msg.text)
+            os.system(sup_to_ref_msg_command)
+
+            if sup_msg.body == 'END':
+                break
+
+            while True:
+                time.sleep(5)
+                ref_msg = client.messages.list()[0]
+                prev_msg = client.messages.list()[1]
+                if ref_msg.direction == 'inbound' \
+                        and prev_msg.direction == 'outbound-api':
+                    break
+
+            translated_ref_msg = translator.translate(
+                ref_msg.body, dest=ENGLISH)
+            ref_to_sup_msg_command = twilio_command.format(
+                account_sid=account_sid,
+                auth_token=auth_token,
+                text_to=sup_number,
+                content=translated_ref_msg.text)
+            os.system(ref_to_sup_msg_command)
+
+            if ref_msg.body == 'END':
+                break
